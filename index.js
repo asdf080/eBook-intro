c39a468d1b54794719585dd7995122e212913839
Dahee Lee <ai04120@naver.com>
2023-12-17 00:10:03 +0900

    asdf

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 window.addEventListener('scroll', () => {
   var FloatBtn = document.querySelector('.FloatBtn');
 
-  // 100px 이상 스크롤 시 버튼 표시
-  if (window.pageYOffset > 230) { 
+  if (window.pageYOffset > 160) { 
     FloatBtn.style.display = 'block';
   } else {
     FloatBtn.style.display = 'none';
   }
 
-  if (window.pageYOffset > 850) {
+  if (window.pageYOffset > 800) {
     FloatBtn.classList.add('fixed');
   } else {
     FloatBtn.classList.remove('fixed');
   }
 });
 
+document.addEventListener('scroll', function() {
+  for(let p of document.querySelectorAll('article p')){
+    if (p.getBoundingClientRect().top - window.innerHeight+100 <= 0) {
+      p.classList.add('visible');
+    } else {
+      p.classList.remove('visible');
+    }
+  }
+});
